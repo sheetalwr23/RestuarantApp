d08c7c122d3b4f1db9fee644a6c7e9a12d509688
Sheetal <Sheetalwr23@gmail.com>
2024-02-22 14:41:58 +0530

    task 8 deleting/removing items from cart

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,29 +1,49 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import classes from "./Cart.module.css";
 import Modal from "../UI/Modal";
 import CartContext from "../../Store/CartContext";
+import CartItem from "./CartItem";
 
 const Cart = (props) => {
   const cartCtx = useContext(CartContext);
+  const [total, setTotal] = useState(0);
 
-  const cartItems = (
-    <ul className={classes["cart-items"]}>
-      {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }]
-        .concat(cartCtx.items)
-        .map((item) => (
-          <li key={item.id}>
-            Item Name: {item.name}, Quantity: {item.quantity}
-          </li>
-        ))}
-    </ul>
-  );
+  useEffect(() => {
+    const sum = cartCtx.items.reduce((accumulator, item) => {
+      return accumulator + item.amount * item.quantity;
+    }, 0);
+
+    setTotal(sum);
+  }, [cartCtx.items]);
+
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
+
+  const cartItemAddHandler = (id) => {
+    cartCtx.addItem({ ...cartCtx.items.find((item) => item.id === id) });
+  };
+
+  const cartItems = cartCtx.items.map((item) => (
+    <CartItem
+      key={item.id}
+      name={item.name}
+      amount={item.amount}
+      price={item.price}
+      quantity={item.quantity}
+      onRemove={cartItemRemoveHandler.bind(null, item.id)}
+      onAdd={cartItemAddHandler.bind(null, item.id)}
+    />
+  ));
+
+  let totalAmount = `$${total.toFixed(2)}`;
 
   return (
     <Modal onClose={props.onClose}>
-      {cartItems}
+      <ul className={classes["cart-items"]}>{cartItems}</ul>
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>{`$${cartCtx.totalAmount.toFixed(2)}`}</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes["button--alt"]} onClick={props.onClose}>
--- src/Components/Store/CartProvider.js
+++ src/Components/Store/CartProvider.js
@@ -2,24 +2,35 @@ import { useState } from "react";
 import CartContext from "./CartContext";
 
 const CartProvider = (props) => {
-  const [items, updateItems] = useState([]);
+  const [items, setItems] = useState([]);
 
   const addItemToCartHandler = (item) => {
-    updateItems([...items, item]);
-    console.log("inside addItemCartHandler", cartContext);
+    const existingItemIndex = items.findIndex(
+      (cartItem) => cartItem.name === item.name
+    );
+
+    if (existingItemIndex !== -1) {
+      const updatedItems = [...items];
+      updatedItems[existingItemIndex].quantity += Number(item.quantity);
+      setItems(updatedItems);
+    } else {
+      setItems((prevItems) => [...prevItems, item]);
+    }
   };
 
-  const removeItemFromCartHandler = (id) => {};
+  const removeItemFromCartHandler = (id) => {
+    setItems((prevItems) => prevItems.filter((item) => item.id !== id));
+  };
 
   const cartContext = {
     items: items,
-    totalAmount: 0,
+    totalAmount: 0, // You may calculate total amount here if needed
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   };
+
   return (
     <CartContext.Provider value={cartContext}>
-      {console.log("inside CartContext.provider", cartContext)}
       {props.children}
     </CartContext.Provider>
   );
